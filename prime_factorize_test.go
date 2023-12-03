2d8f10c61fba43e2a62a4c8109ea88186592eb69
kanaru-ssk <2019january.0131@gmail.com>
2023-12-04 02:00:58 +0900

    fix test

--- prime_factorize_test.go
+++ prime_factorize_test.go
@@ -10,12 +10,12 @@ func TestPrimeFactorize(t *testing.T) {
 		Input    uint32
 		Expected map[uint32]uint32
 	}{
+		{Input: 0, Expected: map[uint32]uint32{}},
 		{Input: 1, Expected: map[uint32]uint32{}},
 		{Input: 2, Expected: map[uint32]uint32{2: 1}},
 		{Input: 12, Expected: map[uint32]uint32{2: 2, 3: 1}},
 		{Input: 53, Expected: map[uint32]uint32{53: 1}},
 		{Input: 4876, Expected: map[uint32]uint32{2: 2, 23: 1, 53: 1}},
-		{Input: 0, Expected: map[uint32]uint32{}},
 	}
 
 	for _, s := range samples {
