1c617347c3e616db806282bef4442aee112b5baf
kanaru-ssk <2019january.0131@gmail.com>
2023-12-03 20:09:01 +0900

    add dynamic_programming

--- dynamic_programming.go
+++ dynamic_programming.go
@@ -1,42 +1,15 @@
 package main
 
-type Item struct {
-	Id, Cost, Value uint32
-}
+func DynamicProgramming[T comparable](tableH, tableW uint32, calcCell func(table [][]T, y, x uint32) T) T {
+	table := make([][]T, tableH)
 
-func DynamicProgramming(capacity uint32, items []Item) uint32 {
-	numItem := len(items)
+	for y := uint32(0); y < tableH; y++ {
 
-	// 最大公約数で割ってtableの大きさを最適化
-	costs := make([]uint32, numItem)
-	for i := range costs {
-		costs[i] = items[i].Cost
-	}
-	gcd := GcdVec(append(costs, capacity))
-	capacity /= gcd
-	for i := range items {
-		items[i] = Item{Id: items[i].Id, Cost: items[i].Cost / gcd, Value: items[i].Value}
-	}
-
-	table := make([][]uint32, numItem)
-	for i := range table {
-		table[i] = make([]uint32, capacity+1)
-	}
-
-	for i := 1; i < numItem; i++ {
-		for j := uint32(0); j <= capacity; j++ {
-			if items[i].Cost > j {
-				// items[i]が入らない場合 : 上のマスを代入
-				table[i][j] = table[i-1][j]
-			} else {
-				// items[i]が入る場合 : 上のマスと比較して大きい方を代入
-				table[i][j] = Max(
-					table[i-1][j-items[i].Cost]+items[i].Value,
-					table[i-1][j],
-				)
-			}
+		for x := uint32(0); x < tableW; x++ {
+			cell := calcCell(table, y, x)
+			table[y] = append(table[y], cell)
 		}
 	}
 
-	return table[numItem-1][capacity]
+	return table[tableH-1][tableW-1]
 }
--- dynamic_programming_test.go
+++ dynamic_programming_test.go
@@ -4,48 +4,87 @@ import (
 	"testing"
 )
 
-func TestBinPacking(t *testing.T) {
-	items := []Item{
-		{Id: 1, Cost: 150, Value: 150},
-		{Id: 2, Cost: 200, Value: 200},
-		{Id: 3, Cost: 250, Value: 250},
-		{Id: 4, Cost: 200, Value: 200},
-		{Id: 5, Cost: 50, Value: 50}}
-
-	capacity1 := uint32(500)
-	result1 := DynamicProgramming(capacity1, items)
-	if result1 != 500 {
-		t.Fatalf("error")
+func TestProgrammingKnapsack(t *testing.T) {
+	type Item struct {
+		Id, Cost, Value uint32
 	}
-	if result1 == capacity1 != true {
-		t.Fatalf("error")
-	}
-
-	capacity2 := uint32(720)
-	result2 := DynamicProgramming(capacity2, items)
-	if result2 != 700 {
-		t.Fatalf("error")
-	}
-	if result2 == capacity2 != false {
-		t.Fatalf("error")
-	}
-}
-
-func TestKnapsack(t *testing.T) {
 	items := []Item{
 		{Id: 1, Cost: 150, Value: 4},
 		{Id: 2, Cost: 200, Value: 7},
 		{Id: 3, Cost: 250, Value: 8},
 		{Id: 4, Cost: 200, Value: 5},
 		{Id: 5, Cost: 50, Value: 1}}
+	calcCell := func(table [][]uint32, y, x uint32) uint32 {
+		if y == 0 {
+			return 0
+		}
+		if x < items[y-1].Cost {
+			return table[y-1][x]
+		}
+		return Max(table[y-1][x-items[y-1].Cost]+items[y-1].Value, table[y-1][x])
+	}
 
 	capacity1 := uint32(500)
-	if DynamicProgramming(capacity1, items) != 16 {
+	if DynamicProgramming(uint32(len(items)+1), capacity1+1, calcCell) != 16 {
 		t.Fatalf("error")
 	}
 
 	capacity2 := uint32(720)
-	if DynamicProgramming(capacity2, items) != 21 {
+	if DynamicProgramming(uint32(len(items)+1), capacity2+1, calcCell) != 21 {
 		t.Fatalf("error")
+	}
+}
+
+func TestBinPacking(t *testing.T) {
+
+	capacity := uint32(10)
+	items := []uint32{4, 7, 8, 5, 1}
+	calcCell := func(table [][]bool, y, x uint32) bool {
+		if y == 0 {
+			return x == 0
+		}
+		if table[y-1][x] {
+			return true
+		}
+		if x < items[y-1] {
+			return false
+		}
+		return table[y-1][x-items[y-1]]
+	}
+
+	if DynamicProgramming(uint32(len(items)+1), capacity+1, calcCell) != true {
+		t.Fatalf("error")
+	}
+}
+
+func TestFibonacci(t *testing.T) {
+	calcCell := func(table [][]uint32, y, x uint32) uint32 {
+		if x <= 0 {
+			return 0
+		}
+		if x <= 2 {
+			return 1
+		}
+		return table[y][x-1] + table[y][x-2]
+	}
+
+	samples := []struct {
+		Input    uint32
+		Expected uint32
+	}{
+		{Input: 0, Expected: 0},
+		{Input: 1, Expected: 1},
+		{Input: 2, Expected: 1},
+		{Input: 3, Expected: 2},
+		{Input: 4, Expected: 3},
+		{Input: 5, Expected: 5},
+		{Input: 6, Expected: 8},
+		{Input: 20, Expected: 6765},
+	}
+
+	for _, s := range samples {
+		if actual := DynamicProgramming(1, s.Input+1, calcCell); actual != s.Expected {
+			t.Errorf("Abs(%v) = %v, want %v", s.Input, actual, s.Expected)
+		}
 	}
 }
